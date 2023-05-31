583046c09236ade7ff272a5ed826699a2a15501f
jcruiz <artur.j.18@hotmail.com>
2023-05-30 23:27:54 -0500

    primer commit

---
+++ ejercicios.js
@@ -0,0 +1,12 @@
+
+
+let respuestaNumero1 = prompt("¿eres bellisimo/a?")
+
+if(respuestaNumero1 === 'si'){
+    console.log('ciertamente')
+}else if(respuestaNumero1 === 'no'){
+    console.log('no te creo')
+};
+
+
+prompt("ingresa tu nombre");
